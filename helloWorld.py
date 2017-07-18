68bca6aa884f025a59c8b5f5fa048fa32b4dea20
Maxence <maxencedumestre@hotmail.com>
2017-07-18 10:06:54 -0400

    Changements :)

--- helloWorld.py
+++ helloWorld.py
@@ -1,3 +1,21 @@
 print('Hello World!')
 
 #salut Max!
+
+for i in range(0,5):
+    print("Hello Félix!")
+
+    print("Are you ready kids? Aye Aye Captain")
+    print("Oohh...")
+    print("Who lives in a pineapple under the sea?")
+    print("Spongebob Squarepants")
+    print("and yellow and porous is he!")
+    print("Spongebob Squarepants")
+    print("Nautical nonsense be something you wish")
+    print("Spongebob Squarepants")
+    print("drop on the deck and flop like a fish!")
+    print("Spongebob Squarepants")
+    print("READY?")
+    print("Spongebob Squarepants")
+    print("Spongebob Squarepants")
+    print("Spongebob Squarepants")
